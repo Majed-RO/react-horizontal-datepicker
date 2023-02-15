60111f13d0aefffd3d4321bff13f5ded1f867012
Webdev Majed <webdev.majed@gmail.com>
2023-02-15 17:48:45 +0200

    Date view and css fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import './App.css';
-import { DatePicker } from "./components/DatePicker";
+import { DatePicker } from './components/DatePicker';
 
 function App() {
-  const selectedDay = (val) =>{
-      console.log(val)
-  };
+	const selectedDay = val => {
+		console.log(val);
+	};
 
-  const startDate = new Date(2010, 0, 1);
+	const startDate = new Date(2010, 0, 1);
 
-  return (
-    <div className="App">
-        <DatePicker startDate={startDate} 
+	return (
+		<div className="App">
+			{/* <DatePicker startDate={startDate} 
                     days={366 * 25}
                     type="month"
                     selectDate={new Date(2021, 9, 1)}
                     getSelectedDay={selectedDay} 
                     labelFormat={"MMMM yyyy"} 
-                    color={"#374e8c"}/>
-    </div>
-  );
+                    color={"#374e8c"}/> */}
+			<DatePicker
+				getSelectedDay={selectedDay}
+				endDate={100}
+				selectDate={new Date('2020-04-30')}
+				labelFormat={'MMMM'}
+				color={'#ffffff'}
+				btnStyles={{ background: 'transparent' }}
+				selectedDayItemStyles={{ fontSize: '0.2em' }}
+				selectedDayStyles={{
+					margin: '6px 0 0 0',
+					color: 'white'
+				}}
+				selectedDateStyles={{
+					border: '1px solid white',
+					borderRadius: '50%',
+					padding: '4px',
+					backgroundColor: 'white',
+					color: 'red',
+					fontSize: '0.94rem'
+				}}
+				arrowsStyles={{ fill: 'white' }}
+			/>
+		</div>
+	);
 }
 
 export default App;
--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,52 +1,98 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { addDays } from "date-fns";
-import React from "react";
-import hexToRgb from "../global/helpers/hexToRgb";
-import styles from "./DatePicker.module.css"
-import { DateView } from "./DateView";
+import { addDays } from 'date-fns';
+import React from 'react';
+import hexToRgb from '../global/helpers/hexToRgb';
+import styles from './DatePicker.module.css';
+import { DateView } from './DateView';
 import { MonthView } from './MonthView';
 
-const DatePicker = (props) => {
-    const next = (event) => {
-        event.preventDefault();
-        const e = document.getElementById('container');
-        const width = e ? e.getBoundingClientRect().width : null;
-        e.scrollLeft += width - 60;
-    };
+const DatePicker = props => {
+	const next = event => {
+		event.preventDefault();
+		const e = document.getElementById('container');
+		const width = e ? e.getBoundingClientRect().width : null;
+		e.scrollLeft += width - 60;
+	};
 
-    const prev = (event) => {
-        event.preventDefault();
-        const e = document.getElementById('container');
-        const width = e ? e.getBoundingClientRect().width : null;
-        e.scrollLeft -= width - 60;
-    };
+	const prev = event => {
+		event.preventDefault();
+		const e = document.getElementById('container');
+		const width = e ? e.getBoundingClientRect().width : null;
+		e.scrollLeft -= width - 60;
+	};
 
-    const primaryColor = props.color? (props.color.indexOf("rgb") > 0?props.color:hexToRgb(props.color)):'rgb(54, 105, 238)';
+	const primaryColor = props.color
+		? props.color.indexOf('rgb') > 0
+			? props.color
+			: hexToRgb(props.color)
+		: 'rgb(54, 105, 238)';
 
-    const startDate = props.startDate || new Date();
-    const lastDate = addDays(startDate, props.days || 90);
+	const startDate = props.startDate || new Date();
+	const lastDate = addDays(startDate, props.days || 90);
 
-    let buttonzIndex = {zIndex: 2};
-    let buttonStyle = {background: primaryColor};
-    let Component = DateView;
+	let buttonzIndex = { zIndex: 2 };
+	let buttonStyle = { background: primaryColor, ...props.btnStyles };
+	let Component = DateView;
 
-    if (props.type === "month") {
-        buttonzIndex = {zIndex: 5};
-        Component = MonthView;
-        buttonStyle = {background: primaryColor, marginBottom: "5px"};
-    }
+	if (props.type === 'month') {
+		buttonzIndex = { zIndex: 5 };
+		Component = MonthView;
+		buttonStyle = { background: primaryColor, marginBottom: '5px' };
+	}
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.buttonWrapper} style={buttonzIndex}>
-                <button className={styles.button} style={buttonStyle} onClick={prev}>&lt;</button>
-            </div>
-            <Component {...props} primaryColor={primaryColor} startDate={startDate} lastDate={lastDate}/>
-            <div className={styles.buttonWrapper} style={buttonzIndex}>
-                <button className={styles.button} style={buttonStyle} onClick={next}>&gt;</button>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className={styles.container}>
+			<div
+				className={styles.buttonWrapper}
+				style={buttonzIndex}
+			>
+				<button
+					className={styles.button}
+					style={buttonStyle}
+					onClick={prev}
+				>
+					<svg
+						style={props.arrowsStyles}
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+					>
+						<path d="M15.293 3.293 6.586 12l8.707 8.707 1.414-1.414L9.414 12l7.293-7.293-1.414-1.414z" />
+					</svg>
+				</button>
+			</div>
+			<Component
+				{...props}
+				primaryColor={primaryColor}
+				startDate={startDate}
+				lastDate={lastDate}
+				selectedContainerStyles={
+					props.selectedContainerStyles
+				}
+				selectedDateStyles={props.selectedDateStyles}
+				selectedDayStyles={props.selectedDayStyles}
+			/>
+			<div
+				className={styles.buttonWrapper}
+				style={buttonzIndex}
+			>
+				<button
+					className={styles.button}
+					style={buttonStyle}
+					onClick={next}
+				>
+					<svg
+						style={props.arrowsStyles}
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+					>
+						<path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" />
+					</svg>
+				</button>
+			</div>
+		</div>
+	);
+};
 
-export { DatePicker }
+export { DatePicker };
